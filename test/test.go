9739ea1758381349f1231cc14061619196124638
ldb <853126656@qq.com>
2018-10-23 15:33:12 +0800

    test about session

---
+++ test/test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int16 = 32767
+	fmt.Println(i)
+	fmt.Println(i << 1)
+}
--- test/web/main/homepage.go
+++ test/web/main/homepage.go
@@ -5,9 +5,19 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"text/template"
 
 	"github.com/ldblovepeace/test/web/action"
+	"github.com/ldblovepeace/test/web/common/session"
+	_ "github.com/ldblovepeace/test/web/common/session/providers/memory"
 )
+
+var globalSessions *session.Manager
+
+//然后在init函数中初始化
+func init() {
+	globalSessions, _ = session.NewManager("memory", "gosessionid", 3600)
+}
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm() //解析url传递的参数，对于POST则解析响应包的主体（request body）
@@ -23,9 +33,25 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "HOMEPAGE") //这个写入到w的是输出到客户端的
 }
 
+func logins(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	r.ParseForm()
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("../HTML/login.html")
+		w.Header().Set("Content-Type", "text/html")
+		t.Execute(w, sess.Get("username"))
+	} else {
+		sess.Set("username", r.Form["username"])
+		value, _ := sess.Get("username").(interface{})
+		fmt.Println(value)
+		http.Redirect(w, r, "/", 302)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", homepage)          //设置访问的路由
 	http.HandleFunc("/login", action.Login) //设置访问的路由
+	http.HandleFunc("/logins", logins)      //test session
 	http.HandleFunc("/upload", action.Upload)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
